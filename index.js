e07471a69189ff8d3fe3d278d1846311e6ac169c
Rki0 <pky00823@gmail.com>
2022-07-17 17:49:39 +0900

    NODE_ENV

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!~~안녕하세요~~Heroku");
+  // res.send("Hello World!~~안녕하세요~~Heroku");
+  res.send(`Hello World!~~안녕하세요~~Heroku~${process.env.NODE_ENV}`);
 });
 
 app.listen(port, () => {
